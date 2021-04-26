4e2c471c808b865a53a543055e11a0309ae022d2
mrmoi2000 <46544861+mrmoi2000@users.noreply.github.com>
2021-04-26 11:44:54 -0700

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var http = require('http');
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   text = "Running Node.js:" + process.versions.node
-  text += "Mongo Servers: " + process.env.MONGODB
+  //text += "Mongo Servers: " + process.env.MONGODB
 
   response.end(text);
 
